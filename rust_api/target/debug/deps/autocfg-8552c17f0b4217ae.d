02ef0897db2da2928f1d498b2947333e9cfb241b
spedmonkey <spedcr@gmail.com>
2024-06-02 14:44:01 +1200

    test

---
+++ rust_api/target/debug/deps/autocfg-8552c17f0b4217ae.d
@@ -0,0 +1,10 @@
+D:\PYTHON\api\rust_api\rust_api\target\debug\deps\libautocfg-8552c17f0b4217ae.rmeta: C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs
+
+D:\PYTHON\api\rust_api\rust_api\target\debug\deps\libautocfg-8552c17f0b4217ae.rlib: C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs
+
+D:\PYTHON\api\rust_api\rust_api\target\debug\deps\autocfg-8552c17f0b4217ae.d: C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs
+
+C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs:
+C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs:
+C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs:
+C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs:
---
+++ rust_api/target/debug/deps/smallvec-8afa56497c214ddd.d
@@ -0,0 +1,7 @@
+D:\PYTHON\api\rust_api\rust_api\target\debug\deps\libsmallvec-8afa56497c214ddd.rmeta: C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.13.2\src\lib.rs
+
+D:\PYTHON\api\rust_api\rust_api\target\debug\deps\libsmallvec-8afa56497c214ddd.rlib: C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.13.2\src\lib.rs
+
+D:\PYTHON\api\rust_api\rust_api\target\debug\deps\smallvec-8afa56497c214ddd.d: C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.13.2\src\lib.rs
+
+C:\Users\cruss\.cargo\registry\src\index.crates.io-6f17d22bba15001f\smallvec-1.13.2\src\lib.rs:
